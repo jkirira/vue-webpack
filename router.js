1f5eb5664433a873d951eca41fba5b39a5aa94fb
jkirira <jkirira@cytonn.com>
2023-10-30 17:00:20 +0300

    fin changes.

--- router.js
+++ router.js
@@ -7,11 +7,7 @@ import Reminders from "/views/Reminders";
 import ReminderDetails from "/views/ReminderDetails";
 
 
-// 2. Define some routes
-// Each route should map to a component. The "component" can
-// either be an actual component constructor created via
-// `Vue.extend()`, or just a component options object.
-// We'll talk about nested routes later.
+
 const routes = [
     { path: '/', component: Home, name: 'home' },
     { path: '/tasks', component: Tasks, name: 'tasks', props: true },
@@ -30,8 +26,7 @@ const routes = [
     }
 ]
 
-// 3. Create the views instance and pass the `routes` option
-// You can pass in additional options here, but let's
+
 const router = new VueRouter({
     routes, // short for `routes: routes`
     mode: 'history'
